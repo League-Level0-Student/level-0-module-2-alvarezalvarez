b895b4b4a74e7fdc9c60959be172fe65628e2f14
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-03 13:54:20 -0700

    Week 5

---
+++ src/methods/Lottery.java
@@ -0,0 +1,21 @@
+package methods;
+
+import java.util.Random;
+
+public class Lottery {
+public static void main(String[] args) {
+	Random n = new Random();
+	int nu = n.nextInt(101);
+	int num = n.nextInt(201)-50;
+int numb = n.nextInt(246)-112;
+int numbe = n.nextInt(260)-359;
+int number = n.nextInt(11);
+	
+	
+	
+	
+	
+	
+	
+}
+}
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -12,12 +12,33 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int randomNumber = randomMaker.nextInt(11);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 0) 
+			JOptionPane.showMessageDialog(null, "You are great");
+		if (randomNumber == 1) 
+			JOptionPane.showMessageDialog(null, "You are cool");
+		if (randomNumber == 2) 
+			JOptionPane.showMessageDialog(null, "You are awesome");
+		if (randomNumber == 3) 
+			JOptionPane.showMessageDialog(null, "You are smart");
+		if (randomNumber == 4) 
+			JOptionPane.showMessageDialog(null, "You are nice");
+		if (randomNumber == 5) 
+			JOptionPane.showMessageDialog(null, "You are very nice");
+		if (randomNumber == 6) 
+			JOptionPane.showMessageDialog(null, "You are good");
+		if (randomNumber == 7) 
+			JOptionPane.showMessageDialog(null, "You are a decent human");
+		if (randomNumber == 8) 
+			JOptionPane.showMessageDialog(null, "You look great");
+		if (randomNumber == 9) 
+			JOptionPane.showMessageDialog(null, "You look awesome");
+		if (randomNumber == 10) 
+			JOptionPane.showMessageDialog(null, "Good job");
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
